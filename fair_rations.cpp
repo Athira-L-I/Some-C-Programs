deb8d04ea511ffe4da959947cdb7663e0f02c0ea
Athira L I <Athira-L-I@users.noreply.github.com>
2018-05-01 20:14:33 +0530

    https://www.hackerrank.com/challenges/fair-rations/problem

---
+++ fair_rations.cpp
@@ -0,0 +1,42 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int N, count=0, sum=0;
+    cin >> N;
+    vector<int> B(N);
+    for(int B_i = 0;B_i < N;B_i++){
+       cin >> B[B_i];
+       sum+= B[B_i];
+        if(sum%2==1) count=count+2;
+    }
+    if(sum%2==0)
+        cout<<count<<endl;
+    else
+        cout<<"NO"<<endl;
+    
+    return 0;
+}
